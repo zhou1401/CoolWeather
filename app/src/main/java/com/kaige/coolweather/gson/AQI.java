d93256a3e18493511ae829912cbba1abb4f6408e
zhoukaige <1006654899@qq.com>
2017-04-06 10:48:45 +0800

    完成基本功能

---
+++ app/src/main/java/com/kaige/coolweather/gson/AQI.java
@@ -0,0 +1,13 @@
+package com.kaige.coolweather.gson;
+
+/**
+ * Created by dell on 2017/4/6.
+ */
+
+public class AQI {
+    public AQICity aqiCity;
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+}
